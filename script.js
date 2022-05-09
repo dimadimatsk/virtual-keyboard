bbe2ec5feb98e6192ce3a1d2a4143e9c79ba4cbf
Dmitriy Shilovskiy <dimareectsk@gmail.com>
2022-05-09 14:45:47 +0700

    refactor: apply prettier

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-import { keyboardFragment, keyboardKeys } from "./keyboard_elements.js";
+import { keyboardFragment, keyboardKeys } from './keyboard_elements.js';
 
 // creating non-keyboard elements
-let container = document.createElement("div");
-let title = document.createElement("p");
-let about = document.createElement("p");
-let combination = document.createElement("p");
+let container = document.createElement('div');
+let title = document.createElement('p');
+let about = document.createElement('p');
+let combination = document.createElement('p');
 
-container.classList.add("container");
+container.classList.add('container');
 document.body.appendChild(container);
 
-title.classList.add("keyboard__title");
-title.innerHTML = "Virtual Keyboard";
+title.classList.add('keyboard__title');
+title.innerHTML = 'Virtual Keyboard';
 container.appendChild(title);
 
-about.classList.add("keyboard__about", "text");
-about.innerText = "For Windows OS";
+about.classList.add('keyboard__about', 'text');
+about.innerText = 'For Windows OS';
 
-combination.classList.add("keyboard__combination", "text");
-combination.innerText = "LShift + LCtrl for switch language";
+combination.classList.add('keyboard__combination', 'text');
+combination.innerText = 'LShift + LCtrl for switch language';
 
 container.appendChild(about);
 container.appendChild(combination);
@@ -26,20 +26,20 @@ container.appendChild(combination);
 class Keyboard {
   constructor() {
     this.caps = false;
-    this.lang = localStorage.getItem("language") === "ru" ? "ru" : "en";
+    this.lang = localStorage.getItem('language') === 'ru' ? 'ru' : 'en';
   }
 
   init() {
-    this.keyboard = document.createElement("div");
-    this.keyboard.classList.add("keyboard");
-    let keyboardRow = document.createElement("div");
-    this.area = document.createElement("textarea");
+    this.keyboard = document.createElement('div');
+    this.keyboard.classList.add('keyboard');
+    let keyboardRow = document.createElement('div');
+    this.area = document.createElement('textarea');
 
-    keyboardRow.classList.add("keyboard__row");
-    this.area.classList.add("keyboard__area");
-    this.area.setAttribute("id", "textarea");
-    this.area.setAttribute("rows", "5");
-    this.area.setAttribute("cols", "50");
+    keyboardRow.classList.add('keyboard__row');
+    this.area.classList.add('keyboard__area');
+    this.area.setAttribute('id', 'textarea');
+    this.area.setAttribute('rows', '5');
+    this.area.setAttribute('cols', '50');
 
     container.insertBefore(this.area, about);
 
@@ -52,94 +52,94 @@ class Keyboard {
 
   _capsLockSwitch(shiftKey) {
     let upperIf = (this.caps && !shiftKey) || (!this.caps && shiftKey);
-    let currCase = upperIf ? "toUpperCase" : "toLowerCase";
+    let currCase = upperIf ? 'toUpperCase' : 'toLowerCase';
 
-    Array.from(this.keyboard.querySelectorAll(".keyboard__key")).forEach(
+    Array.from(this.keyboard.querySelectorAll('.keyboard__key')).forEach(
       (key) => {
         if (!keyboardKeys[key.id].func) {
-          if (key.id === "Backquote" && this.lang === "en") {
-            key.textContent = shiftKey ? "~" : "`";
-          } else if (key.id === "Minus") {
-            key.textContent = shiftKey ? "_" : "-";
-          } else if (key.id === "Equal") {
-            key.textContent = shiftKey ? "+" : "=";
-          } else if (key.id === "BracketLeft" && this.lang === "en") {
-            key.textContent = shiftKey ? "{" : "[";
-          } else if (key.id === "BracketRight" && this.lang === "en") {
-            key.textContent = shiftKey ? "}" : "]";
-          } else if (key.id === "Backslash" && this.lang === "en") {
-            key.textContent = shiftKey ? "|" : "\\";
-          } else if (key.id === "Semicolon" && this.lang === "en") {
-            key.textContent = shiftKey ? ":" : ";";
-          } else if (key.id === "Quote" && this.lang === "en") {
+          if (key.id === 'Backquote' && this.lang === 'en') {
+            key.textContent = shiftKey ? '~' : '`';
+          } else if (key.id === 'Minus') {
+            key.textContent = shiftKey ? '_' : '-';
+          } else if (key.id === 'Equal') {
+            key.textContent = shiftKey ? '+' : '=';
+          } else if (key.id === 'BracketLeft' && this.lang === 'en') {
+            key.textContent = shiftKey ? '{' : '[';
+          } else if (key.id === 'BracketRight' && this.lang === 'en') {
+            key.textContent = shiftKey ? '}' : ']';
+          } else if (key.id === 'Backslash' && this.lang === 'en') {
+            key.textContent = shiftKey ? '|' : '\\';
+          } else if (key.id === 'Semicolon' && this.lang === 'en') {
+            key.textContent = shiftKey ? ':' : ';';
+          } else if (key.id === 'Quote' && this.lang === 'en') {
             key.textContent = shiftKey ? '"' : "'";
-          } else if (key.id === "Comma" && this.lang === "en") {
-            key.textContent = shiftKey ? "<" : ",";
-          } else if (key.id === "Period" && this.lang === "en") {
-            key.textContent = shiftKey ? ">" : ".";
-          } else if (key.id === "Slash" && this.lang === "en") {
-            key.textContent = shiftKey ? "?" : "/";
-          } else if (key.id === "Slash" && this.lang === "ru") {
-            key.textContent = shiftKey ? "," : ".";
-          } else if (key.id === "Digit1") {
-            if (this.lang === "en") {
-              key.textContent = shiftKey ? "!" : "1";
+          } else if (key.id === 'Comma' && this.lang === 'en') {
+            key.textContent = shiftKey ? '<' : ',';
+          } else if (key.id === 'Period' && this.lang === 'en') {
+            key.textContent = shiftKey ? '>' : '.';
+          } else if (key.id === 'Slash' && this.lang === 'en') {
+            key.textContent = shiftKey ? '?' : '/';
+          } else if (key.id === 'Slash' && this.lang === 'ru') {
+            key.textContent = shiftKey ? ',' : '.';
+          } else if (key.id === 'Digit1') {
+            if (this.lang === 'en') {
+              key.textContent = shiftKey ? '!' : '1';
             } else {
-              key.textContent = shiftKey ? "!" : "1";
+              key.textContent = shiftKey ? '!' : '1';
             }
-          } else if (key.id === "Digit2") {
-            if (this.lang === "en") {
-              key.textContent = shiftKey ? "@" : "2";
+          } else if (key.id === 'Digit2') {
+            if (this.lang === 'en') {
+              key.textContent = shiftKey ? '@' : '2';
             } else {
-              key.textContent = shiftKey ? '"' : "2";
+              key.textContent = shiftKey ? '"' : '2';
             }
-          } else if (key.id === "Digit3") {
-            if (this.lang === "en") {
-              key.textContent = shiftKey ? "#" : "3";
+          } else if (key.id === 'Digit3') {
+            if (this.lang === 'en') {
+              key.textContent = shiftKey ? '#' : '3';
             } else {
-              key.textContent = shiftKey ? "№" : "3";
+              key.textContent = shiftKey ? '№' : '3';
             }
-          } else if (key.id === "Digit4") {
-            if (this.lang === "en") {
-              key.textContent = shiftKey ? "$" : "4";
+          } else if (key.id === 'Digit4') {
+            if (this.lang === 'en') {
+              key.textContent = shiftKey ? '$' : '4';
             } else {
-              key.textContent = shiftKey ? ";" : "4";
+              key.textContent = shiftKey ? ';' : '4';
             }
-          } else if (key.id === "Digit5") {
-            if (this.lang === "en") {
-              key.textContent = shiftKey ? "%" : "5";
+          } else if (key.id === 'Digit5') {
+            if (this.lang === 'en') {
+              key.textContent = shiftKey ? '%' : '5';
             } else {
-              key.textContent = shiftKey ? "%" : "5";
+              key.textContent = shiftKey ? '%' : '5';
             }
-          } else if (key.id === "Digit6") {
-            if (this.lang === "en") {
-              key.textContent = shiftKey ? "^" : "6";
+          } else if (key.id === 'Digit6') {
+            if (this.lang === 'en') {
+              key.textContent = shiftKey ? '^' : '6';
             } else {
-              key.textContent = shiftKey ? ":" : "6";
+              key.textContent = shiftKey ? ':' : '6';
             }
-          } else if (key.id === "Digit7") {
-            if (this.lang === "en") {
-              key.textContent = shiftKey ? "&" : "7";
+          } else if (key.id === 'Digit7') {
+            if (this.lang === 'en') {
+              key.textContent = shiftKey ? '&' : '7';
             } else {
-              key.textContent = shiftKey ? "?" : "7";
+              key.textContent = shiftKey ? '?' : '7';
             }
-          } else if (key.id === "Digit8") {
-            if (this.lang === "en") {
-              key.textContent = shiftKey ? "*" : "8";
+          } else if (key.id === 'Digit8') {
+            if (this.lang === 'en') {
+              key.textContent = shiftKey ? '*' : '8';
             } else {
-              key.textContent = shiftKey ? "*" : "8";
+              key.textContent = shiftKey ? '*' : '8';
             }
-          } else if (key.id === "Digit9") {
-            if (this.lang === "en") {
-              key.textContent = shiftKey ? "(" : "9";
+          } else if (key.id === 'Digit9') {
+            if (this.lang === 'en') {
+              key.textContent = shiftKey ? '(' : '9';
             } else {
-              key.textContent = shiftKey ? "(" : "9";
+              key.textContent = shiftKey ? '(' : '9';
             }
-          } else if (key.id === "Digit0") {
-            if (this.lang === "en") {
-              key.textContent = shiftKey ? ")" : "0";
+          } else if (key.id === 'Digit0') {
+            if (this.lang === 'en') {
+              key.textContent = shiftKey ? ')' : '0';
             } else {
-              key.textContent = shiftKey ? ")" : "0";
+              key.textContent = shiftKey ? ')' : '0';
             }
           } else {
             key.textContent = key.textContent[currCase]();
@@ -172,9 +172,9 @@ class Keyboard {
   }
 
   _pressDel() {
-    console.log("Del pressed");
+    console.log('Del pressed');
     if (this.area.selectionStart !== this.area.selectionEnd) {
-      this._writeText("");
+      this._writeText('');
     } else {
       let start = this.area.selectionStart;
 
@@ -187,9 +187,9 @@ class Keyboard {
   }
 
   _pressBackspace() {
-    console.log("Backspace pressed");
+    console.log('Backspace pressed');
     if (this.area.selectionStart !== this.area.selectionEnd) {
-      this._writeText("");
+      this._writeText('');
     } else {
       let start = Math.max(0, this.area.selectionStart - 1);
       console.log(start);
@@ -204,69 +204,69 @@ class Keyboard {
   }
 
   _addListeners() {
-    document.addEventListener("keydown", (event) => {
+    document.addEventListener('keydown', (event) => {
       event.stopImmediatePropagation();
 
       let key = document.getElementById(event.code);
       if (!key) {
-        console.log("test");
+        console.log('test');
         event.preventDefault();
         return;
       }
 
-      if (event.code === "CapsLock" && !event.repeat) {
+      if (event.code === 'CapsLock' && !event.repeat) {
         event.preventDefault();
         this.caps = !this.caps;
         if (this.caps) {
-          key.classList.add("active");
+          key.classList.add('active');
         } else {
-          key.classList.remove("active");
+          key.classList.remove('active');
         }
         this._capsLockSwitch(event.shiftKey);
       } else if (event.metaKey) {
-        key.classList.add("active");
+        key.classList.add('active');
       } else {
-        key.classList.add("active");
+        key.classList.add('active');
         if (event.shiftKey && event.altKey && !event.repeat) {
           event.preventDefault();
-          this.lang = this.lang === "ru" ? "en" : "ru";
+          this.lang = this.lang === 'ru' ? 'en' : 'ru';
           console.log(this.lang);
           this._switchLang(this.lang);
-          localStorage.setItem("language", this.lang);
+          localStorage.setItem('language', this.lang);
         } else if (!keyboardKeys[event.code].func) {
           event.preventDefault();
           this._writeText(key.textContent);
         } else if (
-          (event.code === "ShiftRight" || event.code === "ShiftLeft") &&
+          (event.code === 'ShiftRight' || event.code === 'ShiftLeft') &&
           !event.repeat
         ) {
           event.preventDefault();
           this._capsLockSwitch(event.shiftKey);
-        } else if (event.key === "Tab") {
+        } else if (event.key === 'Tab') {
           event.preventDefault();
-          this._writeText("\t");
-        } else if (event.code === "Enter") {
+          this._writeText('\t');
+        } else if (event.code === 'Enter') {
           event.preventDefault();
-          this._writeText("\n");
+          this._writeText('\n');
         } else if (
-          event.code === "ArrowLeft" ||
-          event.code === "ArrowDown" ||
-          event.code === "ArrowRight" ||
-          event.code === "ArrowUp"
+          event.code === 'ArrowLeft' ||
+          event.code === 'ArrowDown' ||
+          event.code === 'ArrowRight' ||
+          event.code === 'ArrowUp'
         ) {
           event.preventDefault();
           this._arrowKeyAction(key.textContent);
-        } else if (event.code === "Backspace") {
+        } else if (event.code === 'Backspace') {
           event.preventDefault();
           this._pressBackspace();
-        } else if (event.code === "Delete") {
+        } else if (event.code === 'Delete') {
           event.preventDefault();
           this._pressDel();
         }
       }
     });
 
-    document.addEventListener("keyup", (event) => {
+    document.addEventListener('keyup', (event) => {
       event.stopImmediatePropagation();
 
       let key = document.getElementById(event.code);
@@ -275,18 +275,18 @@ class Keyboard {
         return;
       }
 
-      if (event.code !== "CapsLock") {
-        key.classList.remove("active");
-        if (event.code === "ShiftRight" || event.code === "ShiftLeft") {
+      if (event.code !== 'CapsLock') {
+        key.classList.remove('active');
+        if (event.code === 'ShiftRight' || event.code === 'ShiftLeft') {
           event.preventDefault();
           this._capsLockSwitch(event.shiftKey);
         }
       }
     });
 
-    this.keyboard.addEventListener("mousedown", (event) => {
+    this.keyboard.addEventListener('mousedown', (event) => {
       this.area.focus();
-      let keyDown = new KeyboardEvent("keydown", {
+      let keyDown = new KeyboardEvent('keydown', {
         bubbles: true,
         cancelable: true,
         code: event.target.id,
@@ -294,9 +294,9 @@ class Keyboard {
       document.dispatchEvent(keyDown);
     });
 
-    this.keyboard.addEventListener("mouseup", (event) => {
+    this.keyboard.addEventListener('mouseup', (event) => {
       this.area.focus();
-      let keyUp = new KeyboardEvent("keyup", {
+      let keyUp = new KeyboardEvent('keyup', {
         bubbles: true,
         cancelable: true,
         code: event.target.id,
@@ -305,7 +305,7 @@ class Keyboard {
     });
   }
   _switchLang(language) {
-    Array.from(this.keyboard.querySelectorAll(".keyboard__key")).forEach(
+    Array.from(this.keyboard.querySelectorAll('.keyboard__key')).forEach(
       (key) => {
         key.textContent = keyboardKeys[key.id][language];
       }
@@ -313,7 +313,7 @@ class Keyboard {
   }
 }
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   const keyboard = new Keyboard();
   keyboard.init();
 });
